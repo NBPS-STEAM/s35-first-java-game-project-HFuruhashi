f9e91667faf127b0a2b56d88aaa0495ad04f8c58
HFuruhashi <37462956+HFuruhashi@users.noreply.github.com>
2020-12-16 12:11:39 -0500

    beginning of the program

--- App.java
+++ App.java
@@ -1,6 +1,22 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int skeletonHp = 150;
+        int playerHp = 100;
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Defeat the Skeleton!");
+
+        while (skeletonHp > 0 || playerHp > 0) {
+          System.out.println("Skeleton's HP: " + skeletonHp);
+          System.out.println("Your HP: " + playerHp);
+          System.out.println("Choose a number!");
+          int action = myObj.nextInt();
+
+          if (action == 1) {
+            skeletonHp -= 10;
+          }
+
+          playerHp -= 15;
+        }
 
 
     }
