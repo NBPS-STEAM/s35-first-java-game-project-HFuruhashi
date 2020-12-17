4c0d1d2f86a3b77dd1bbf6f863e410241446e059
HFuruhashi <37462956+HFuruhashi@users.noreply.github.com>
2020-12-16 23:31:03 -0500

    terminal package experiment

--- App.java
+++ App.java
@@ -1,15 +1,19 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
+
         int skeletonHp = 150;
         int playerHp = 100;
-        Scanner myObj = new Scanner(System.in);
         System.out.println("Defeat the Skeleton!");
 
         while (skeletonHp > 0 || playerHp > 0) {
           System.out.println("Skeleton's HP: " + skeletonHp);
           System.out.println("Your HP: " + playerHp);
+          System.out.println("\n What will you do?: <1. Attack>");
           System.out.println("Choose a number!");
-          int action = myObj.nextInt();
+          int action;
+          Scanner sc = new Scanner(System.in);
+          action = sc.nextInt();
 
           if (action == 1) {
             skeletonHp -= 10;
