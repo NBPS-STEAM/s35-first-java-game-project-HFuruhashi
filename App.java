819be7c2f636b393bfbc28cfd78171b885f796eb
HFuruhashi <37462956+HFuruhashi@users.noreply.github.com>
2020-12-17 11:57:41 -0500

    first heal system

--- App.java
+++ App.java
@@ -4,12 +4,14 @@ public class App {
 
         int skeletonHp = 150;
         int playerHp = 100;
-        System.out.println("Defeat the Skeleton!");
+        boolean useHeal = true;
+        int healCount = 0;
 
-        while (skeletonHp > 0 || playerHp > 0) {
+        System.out.println("Defeat the Skeleton!");
+        while (skeletonHp > 0 && playerHp > 0) {
           System.out.println("\nSkeleton's HP: " + skeletonHp);
           System.out.println("Your HP: " + playerHp);
-          System.out.println("\n What will you do?: 1. Attack");
+          System.out.println("\nWhat will you do?: 1. Attack 2. Heal");
           System.out.println("Choose a number!");
           int action;
           Scanner sc = new Scanner(System.in);
@@ -18,13 +20,42 @@ public class App {
           if (action == 1) {
             skeletonHp -= 10;
             System.out.println("\nYou attacked!");
+          } else if (action == 2) {
+            if (useHeal == true) {
+              playerHp += 50;
+              if (playerHp > 100) {
+                playerHp = 100;
+              }
+              useHeal = false;
+              System.out.println("You healed yourself!");
+              System.out.println("Your HP: " + playerHp);
+            } else {
+              System.out.println("\nYou used it in the last turn. Wait until your next turn!");
+            }
+          } else {
+            System.out.println("Invalid Command!");
           }
 
-          System.out.println("\n Skeleton's Multiple Attack!!");
+          System.out.println("\nSkeleton's Multiple Attack!!");
           for (int i = 0; i < 3; i++) {
             playerHp -= 5;
             System.out.println("\nYour HP: " + playerHp);
           }
+
+          if (useHeal == false) {
+            if (healCount == 0) {
+              healCount += 1;
+            } else {
+              useHeal = true;
+              healCount = 0;
+            }
+          }
+        }
+
+        if (skeletonHp == 0 && playerHp > 0) {
+          System.out.println("\nYou defeated the skeleton!!");
+        } else {
+          System.out.println("\nYou lost!");
         }
 
 
